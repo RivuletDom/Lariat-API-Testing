daef4c7e5bbbca3fab78bb1f2fe5e6d19f9ec947
RivuletDom <78263954+RivuletDom@users.noreply.github.com>
2021-02-02 17:39:37 +0530

    final login and token inherited to other apis

--- src/main/java/apitesting/login.java
+++ src/main/java/apitesting/login.java
@@ -9,35 +9,31 @@ import io.restassured.specification.RequestSpecification;
 
 public class login {
 	
- 
+ public static String barriertoken = "";
+	
  @Test
-  public void login_marcom()
-  {   
-		
-		
-		RestAssured.baseURI ="https://marcom20-production.whitelabeliq.net/";
-		 RequestSpecification request = RestAssured.given();
-		 request.header("Content-Type", "application/json");
+  public String login_marcom()
+ {
 
-		 JSONObject requestParams = new JSONObject();
-		 requestParams.put("email",  "marcom.jassi@gmail.com");
-		 requestParams.put("password", "marcom2020");
-		 
-		 request.body(requestParams.toJSONString());
-		 
-		 Response response = request.post("/api/v1/login");
-		 int statusCode = response.getStatusCode();
-		 
-		 System.out.println("The status code recieved: " + statusCode);
-		 
-		 System.out.println("Response body: " + response.getBody().jsonPath().prettify());
-//		 System.out.println("token : " + (response.getBody().jsonPath().get("data.access_token")));
-		 
-		String token = response.getBody().jsonPath().get("data.access_token");
-		 System.out.println("token : " +token);
-		
-		 Assert.assertEquals(statusCode, 200);
-		 
+		RestAssured.baseURI = "https://marcom20-production.whitelabeliq.net/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("email", "marcom.jassi@gmail.com");
+		requestParams.put("password", "marcom2020");
+
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("/api/v1/login");
+		int statusCode = response.getStatusCode();
+
+		System.out.println("The status code recieved: " + statusCode);
+
+		System.out.println("Response body: " + response.getBody().jsonPath().prettify());
+
+		Assert.assertEquals(statusCode, 200);
+		return response.getBody().jsonPath().get("data.access_token");
 
 	}
 }
