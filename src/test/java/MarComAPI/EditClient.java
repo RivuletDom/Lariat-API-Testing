ad5b2449ad2045109547f53b35366102ba5f0a1a
RivuletDom <78263954+RivuletDom@users.noreply.github.com>
2021-02-18 17:51:13 +0530

    add file

---
+++ src/test/java/MarComAPI/EditClient.java
@@ -0,0 +1,51 @@
+package test.java.MarComAPI;
+
+import org.json.simple.JSONObject;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class EditClient extends login {
+
+	@Test
+	public void EditClientDetails() throws Throwable {
+
+		System.out.println(" *** API: Edit Client ***  \n");
+
+		String token = login_marcom();
+
+		RestAssured.baseURI = "https://marcom20-production.whitelabeliq.net/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		System.out.println("Token : " + token);
+		request.header("Authorization", "Bearer " + token);
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("workspace_id", workspace_id);
+		requestParams.put("client_id", "71");
+		requestParams.put("company_name", "DevOps International");
+		requestParams.put("phone_number", "43432423423");
+		requestParams.put("address_line1", "ahmedabad");
+		requestParams.put("company_email", "devops@gmail.com");
+		requestParams.put("city", "ahmedabad");
+		requestParams.put("state", "Gujarat");
+		requestParams.put("zip_code", "380051");
+
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("api/v1/client/edit?url_workspace_id=278054311");
+		int statusCode = response.getStatusCode();
+
+		System.out.println("The status code recieved: " + statusCode);
+		System.out.println("Response body: " + response.getBody().jsonPath().prettify());
+
+		AssertJUnit.assertEquals(statusCode, 200);
+
+		System.out.println("\n\n ----------------------------------------------- \n\n");
+		Thread.sleep(3000);
+
+	}
+}
