6bba5c992f852bc07b11b1888ee85bbeb1c35859
RivuletDom <78263954+RivuletDom@users.noreply.github.com>
2021-02-22 16:06:57 +0530

    adding_department

---
+++ src/test/java/MarComAPI/ListAllDepartments.java
@@ -0,0 +1,44 @@
+package test.java.MarComAPI;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ListAllDepartments extends login {
+
+	@Test
+	public void ListDepartments() throws Throwable {
+
+		System.out.println(" *** API: List All Departments of workspace***  \n");
+
+		String token = login_marcom();
+
+		RestAssured.baseURI = "https://marcom20-production.whitelabeliq.net/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		System.out.println("Token : " + token);
+		request.header("Authorization", "Bearer " + token);
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("workspace_id", workspace_id);
+
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("api/v1/departments/list?url_workspace_id=278054311");
+		int statusCode = response.getStatusCode();
+
+		System.out.println("The status code recieved: " + statusCode);
+		System.out.println("/n Response body: " + response.getBody().jsonPath().prettify());
+
+		AssertJUnit.assertEquals(statusCode, 200);
+
+		System.out.println("\n\n ----------------------------------------------- \n\n");
+		Thread.sleep(3000);
+
+	}
+}
