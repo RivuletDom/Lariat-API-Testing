ee966140e44464b6567395028074772984cc78f1
RivuletDom <78263954+RivuletDom@users.noreply.github.com>
2021-02-22 16:56:21 +0530

    adding_more_api_list

---
+++ src/test/java/MarComAPI/ListGeneralComment.java
@@ -0,0 +1,46 @@
+package test.java.MarComAPI;
+
+import org.json.simple.JSONObject;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class ListGeneralComment extends login {
+
+	@Test(priority = 2)
+	public void ListingGeneralComment() throws Throwable {
+
+		System.out.println(" *** API: List General Comment in Project *** \n");
+
+		String token = login_marcom();
+
+		RestAssured.baseURI = "https://marcom20-production.whitelabeliq.net/";
+		RequestSpecification request = RestAssured.given();
+		request.header("Content-Type", "application/json");
+		System.out.println("Token : " + token);
+		request.header("Authorization", "Bearer " + token);
+
+		JSONObject requestParams = new JSONObject();
+		requestParams.put("workspace_id", workspace_id);
+		requestParams.put("project_id", "1340298153");
+		requestParams.put("type", "general");
+
+
+		request.body(requestParams.toJSONString());
+
+		Response response = request.post("api/v1/project/comment/general/list?url_workspace_id=278054311");
+		int statusCode = response.getStatusCode();
+
+		System.out.println("The status code recieved: " + statusCode);
+
+		System.out.println("Response body: " + response.getBody().jsonPath().prettify());
+
+		AssertJUnit.assertEquals(statusCode, 200);
+
+		System.out.println("\n\n ------------------------------------------------ \n\n");
+		Thread.sleep(3000); 
+	}
+}
